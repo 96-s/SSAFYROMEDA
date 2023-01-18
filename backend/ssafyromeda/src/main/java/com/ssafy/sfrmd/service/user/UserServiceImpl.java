f6b07fc8114e8555bd2a5772ab478b224e34aaaa
dpdms2148 <dpdms2148@naver.com>
2023-01-18 11:38:28 +0900

    feat:UserServiceImpl 메서드 구현

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/service/user/UserServiceImpl.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/service/user/UserServiceImpl.java
@@ -1,2 +1,32 @@
-package com.ssafy.sfrmd.service.user;public class UserServiceImpl {
+package com.ssafy.sfrmd.service.user;
+
+import com.ssafy.sfrmd.domain.user.User;
+import com.ssafy.sfrmd.domain.user.UserRepository;
+import com.ssafy.sfrmd.dto.user.UserSignUpDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public User sighUpUser(UserSignUpDto userSignUpDto) {
+        User user = User.builder()
+            .userEmail(userSignUpDto.getUserEmail())
+            .userNickname(userSignUpDto.getUserNickName())
+            .build();
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Boolean checkEmail(String userEamil) {
+        return null;
+    }
+
+    @Override
+    public Boolean checkNickname(String userNickname) {
+        return null;
+    }
 }
