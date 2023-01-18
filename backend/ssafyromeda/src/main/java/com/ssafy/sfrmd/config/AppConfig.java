d5dfbd997a3e71092d37e74d38a88fd85fd40882
이창민 <cmlee0913@naver.com>
2023-01-18 14:42:59 +0900

    feat:VideoService 추가

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/config/AppConfig.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/config/AppConfig.java
@@ -18,41 +18,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-//    @Bean
-//    public OpenVidu openVidu(@Value("${openvidu.secret}") String secret,
-//        @Value("${openvidu.url}") String openViduUrl) {
-//        return new OpenVidu(openViduUrl, secret);
-//    }
-
-    OpenVidu openvidu = new OpenVidu("https://localhost:4443/", "MY_SECRET");
-    SessionProperties properties = new SessionProperties.Builder().build();
-    Session session = null;
-
-    {
-        try {
-            session = openvidu.createSession(properties);
-        } catch (OpenViduJavaClientException ex) {
-            throw new RuntimeException(ex);
-        } catch (OpenViduHttpException ex) {
-            throw new RuntimeException(ex);
-        }
+    @Bean
+    public OpenVidu openVidu(@Value("${openvidu.secret}") String secret,
+        @Value("${openvidu.url}") String openViduUrl) {
+        return new OpenVidu(openViduUrl, secret);
     }
-
-    ConnectionProperties connectionProperties = new ConnectionProperties.Builder()
-        .type(ConnectionType.WEBRTC)
-        .role(OpenViduRole.PUBLISHER)
-        .data("user_data")
-        .build();
-
-    Connection connection = null;
-    {
-        try {
-            Connection connection = session.createConnection(connectionProperties);
-        } catch(OpenViduJavaClientException e) {
-            throw new RuntimeException(e);
-        } catch(OpenViduHttpException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    String token = connection.getToken(); // Send this string to the client side
 }
