16d838f53205116813619fc9c3e3980f08dbd1c0
이창민 <cmlee0913@naver.com>
2023-01-18 13:43:18 +0900

    feat:openvidu logic 완성

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/config/AppConfig.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/config/AppConfig.java
@@ -4,6 +4,8 @@ import io.openvidu.java.client.Connection;
 import io.openvidu.java.client.ConnectionProperties;
 import io.openvidu.java.client.ConnectionType;
 import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
 import io.openvidu.java.client.OpenViduRole;
 import io.openvidu.java.client.Session;
 import io.openvidu.java.client.SessionProperties;
@@ -16,19 +18,41 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public OpenVidu openVidu(@Value("${openvidu.secret}") String secret,
-        @Value("${openvidu.url}") String openViduUrl) {
-        return new OpenVidu(openViduUrl, secret);
-    }
+//    @Bean
+//    public OpenVidu openVidu(@Value("${openvidu.secret}") String secret,
+//        @Value("${openvidu.url}") String openViduUrl) {
+//        return new OpenVidu(openViduUrl, secret);
+//    }
+
+    OpenVidu openvidu = new OpenVidu("https://localhost:4443/", "MY_SECRET");
     SessionProperties properties = new SessionProperties.Builder().build();
-    Session session = openVidu().createSession(properties);
+    Session session = null;
+
+    {
+        try {
+            session = openvidu.createSession(properties);
+        } catch (OpenViduJavaClientException ex) {
+            throw new RuntimeException(ex);
+        } catch (OpenViduHttpException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
     ConnectionProperties connectionProperties = new ConnectionProperties.Builder()
         .type(ConnectionType.WEBRTC)
         .role(OpenViduRole.PUBLISHER)
         .data("user_data")
         .build();
 
-    Connection connection = session.createConnection(connectionProperties);
+    Connection connection = null;
+    {
+        try {
+            Connection connection = session.createConnection(connectionProperties);
+        } catch(OpenViduJavaClientException e) {
+            throw new RuntimeException(e);
+        } catch(OpenViduHttpException e) {
+            throw new RuntimeException(e);
+        }
+    }
     String token = connection.getToken(); // Send this string to the client side
 }
