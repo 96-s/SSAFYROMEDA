32bab788290a331dbdbb251317f49bfde68ed392
dpdms2148 <dpdms2148@naver.com>
2023-01-18 14:33:37 +0900

    fix: 오타 수정

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/domain/user/UserRepository.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/domain/user/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    Optional<User> findByUserEamil(String userEmail);
+    Optional<User> findByUserEmail(String userEmail);
     Optional<User> findByUserNickname(String userNickname);
     Optional<User> findByUserRefreshToken(String userRefreshToken);
 }
