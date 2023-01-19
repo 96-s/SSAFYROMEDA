6462ad87c4012bb67a09e40a545bc28e4a8b0190
haesom <fliamento@naver.com>
2023-01-19 17:34:43 +0900

    feat: store 기본 구축

--- frontend/src/store/AuthSlice.js
+++ frontend/src/store/AuthSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@/reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialAuthState = {
   // 회원가입(닉네임 등록)
@@ -7,12 +7,19 @@ const initialAuthState = {
   },
   ProfileInfo: [],
   isAuth: null, // 로그인 유무
+  error: null, // 에러 유무
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState: initialAuthState,
-  reducers: {},
+  reducers: {
+    // form 업데이트
+    changeField(state, action) {
+      const { form, key, value } = action.payload;
+      state[form][key] = value;
+    },
+  },
 });
 
 export const authActions = authSlice.actions;
