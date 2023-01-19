2257b2a0af4c1aa829e86fe7313b7d9b38624152
dpdms2148 <dpdms2148@naver.com>
2023-01-19 17:35:54 +0900

    feat:OAuth2를 모두 Auth로 변경 및 디렉토리 구조 변경

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/service/user/AuthService.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/service/user/AuthService.java
@@ -1,9 +1,9 @@
 package com.ssafy.sfrmd.service.user;
 
-import com.ssafy.sfrmd.domain.user.OAuth2User;
+import com.ssafy.sfrmd.domain.user.auth.AuthUser;
 import com.ssafy.sfrmd.domain.user.User;
 import com.ssafy.sfrmd.domain.user.UserRepository;
-import com.ssafy.sfrmd.dto.user.oauth.OAuth2Dto;
+import com.ssafy.sfrmd.dto.user.auth.AuthDto;
 import java.util.Collections;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -12,25 +12,24 @@ import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserServ
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+public class AuthService implements OAuth2UserService<OAuth2UserRequest, AuthUser> {
 
     private final UserRepository userRepository;
 
     @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+    public AuthUser loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         /**
          * DefaultOAuth2UserService 객체를 생성하여, loadUser(userRequest)를 통해 DefaultOAuth2User 객체를 생성 후 반환
          * DefaultOAuth2UserService의 loadUser()는 소셜 로그인 API의 사용자 정보 제공 URI로 요청을 보내서
          * 사용자 정보를 얻은 후, 이를 통해 DefaultOAuth2User 객체를 생성 후 반환한다.
          * 결과적으로, OAuth2User는 OAuth 서비스에서 가져온 유저 정보를 담고 있는 유저
          */
-        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
-        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        OAuth2UserService<OAuth2UserRequest, AuthUser> delegate = new DefaultOAuth2UserService();
+        AuthUser oAuth2User = delegate.loadUser(userRequest);
 
         /**
          * userRequest에서 registrationId 추출 후 registrationId으로 SocialType 저장
@@ -42,19 +41,19 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
         Map<String, Object> attributes = oAuth2User.getAttributes(); // 소셜 로그인에서 API가 제공하는 userInfo의 Json 값(유저 정보들)
 
         // socialType에 따라 유저 정보를 통해 OAuthAttributes 객체 생성
-        OAuth2Dto oauth2Dto = OAuth2Dto.ofKakao(userNameAttributeName, attributes);
+        AuthDto oauth2Dto = AuthDto.ofKakao(userNameAttributeName, attributes);
 
         User createdUser = getUser(oauth2Dto); // getUser() 메소드로 User 객체 생성 후 반환
 
         // DefaultOAuth2User를 구현한 CustomOAuth2User 객체를 생성해서 반환
-        return new OAuth2User(
+        return new AuthUser(
             Collections.singleton(new SimpleGrantedAuthority(createdUser.getUserRole().getRole())),
             attributes,
             oauth2Dto.getNameAttributeKey(),
             createdUser.getUserRole());
     }
 
-    private User getUser(OAuth2Dto oauth2Dto) {
+    private User getUser(AuthDto oauth2Dto) {
         return null;
     }
 }
