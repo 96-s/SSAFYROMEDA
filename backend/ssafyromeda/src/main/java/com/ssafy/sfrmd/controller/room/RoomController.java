a96cf92dcd5e23425cd99e32f78da1b33fdb344a
김성욱 <kimtjddnr2542naver.com>
2023-01-20 15:08:45 +0900

    feat : 방 삭제 controller 추가

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/controller/room/RoomController.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/controller/room/RoomController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,14 @@ public class RoomController {
         if(room.getRoomSeq() != null){
             return new ResponseEntity<>(room, HttpStatus.valueOf(200));
         }
+        return new ResponseEntity<>(null, HttpStatus.valueOf(400));
+    }
+
+    @DeleteMapping("{roomCode}")
+    public ResponseEntity<? extends Object> deleteRoom(@PathVariable("roomCode") String roomCode){
+
+
+
         return new ResponseEntity<>(null, HttpStatus.valueOf(400));
     }
 }
