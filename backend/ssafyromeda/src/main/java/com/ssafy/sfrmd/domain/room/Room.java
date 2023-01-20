d799d4c8965ea1be0cd8524e8d5e6c69204bdba5
김성욱 <kimtjddnr2542naver.com>
2023-01-20 11:17:13 +0900

    feat : 방 코드 컬럼 추가

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/domain/room/Room.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/domain/room/Room.java
@@ -11,17 +11,21 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name="room")
+// 방 정보
 public class Room {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="room_code")
-    private Long roomCode;
+    @Column(name="room_seq") // 방 auto_increment
+    private Long roomSeq;
 
-    @Column(name="room_host")
+    @Column(name="room_code") // 방 코드
+    private String roomCode;
+
+    @Column(name="room_host") // 방장
     private long roomHost;
 
-    @Column(name="room_cnt")
+    @Column(name="room_cnt") // 방 인원
     private int roomCnt;
 
 }
