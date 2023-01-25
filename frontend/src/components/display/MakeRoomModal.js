7d7b7a8060345ebb3b36ca68bd526f43cc46018b
jungeun park <pyooooong@gmail.com>
2023-01-25 12:41:49 +0900

    feat:로비/ 프로필 모달 추가

---
+++ frontend/src/components/display/MakeRoomModal.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import 'components/common/modal.css';
+import MyButton from "components/common/Button";
+import styled from "styled-components";
+import { useState } from 'react';
+
+const MakeRoomDiv = styled.div`
+  flex: auto;
+`;
+
+const MakeRoomModal = (props) => {
+  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+  const { open, close, header } = props;
+  const [isMade, setIsMade] = useState(false);
+  // const toggleIsMade = () => {
+  //   ;
+  // }
+
+  return (
+    // 모달이 열릴때 openModal 클래스가 생성된다.
+    <div className={open ? "openModal modal" : "modal"}>
+      {open ? (
+        <section>
+          <header>
+            {header}
+            {/* <button className="close" onClick={close}>
+              &times;
+            </button> */}
+          </header>
+          <main>
+            <MakeRoomDiv>
+              {!isMade &&
+                <MyButton
+                  type={"Korean"}
+                  className={"is-primary"}
+                  text={"초대코드 생성"}
+                  onClick={() => setIsMade(!isMade)}
+                />
+              }
+              <div>
+                {/* <input className="editNickname" type="text"></input> */}
+              </div>
+              {isMade && 
+                <div>
+
+                  <span>{'초대코드'}</span>
+                  <MyButton
+                  type={"Korean"}
+                  className={"is-primary"}
+                  text={"입장"}
+                  onClick={"임시"}
+                  />
+                </div>
+              }
+            </MakeRoomDiv>
+          </main>
+          <footer>
+            <button className="close" onClick={close}>
+              close
+            </button>
+          </footer>
+        </section>
+      ) : null}
+    </div>
+  );
+};
+export default MakeRoomModal;
