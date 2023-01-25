af2046c9db9d1cbcf6ff2ae2accbaeb7036d6515
dpdms <dpdms2148@naver.com>
2023-01-26 01:31:29 +0900

    feat : AuthenticationFailureHandler 작성

---
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/handler/AuthenticationFailureHandlerImpl.java
@@ -0,0 +1,18 @@
+package com.ssafy.sfrmd.handler;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthenticationFailureHandlerImpl implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        ApiResponse.error(response, ApiResponseType.FORBIDDEN_RESPONSE, "로그인 실패");
+    }
+}
