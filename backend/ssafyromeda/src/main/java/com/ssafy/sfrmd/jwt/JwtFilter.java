853ef2b722d2a73a2d5f2c1adc04c39145a35e18
dpdms2148 <dpdms2148@naver.com>
2023-01-25 12:49:04 +0900

    feat: 어노테이션 추가

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/jwt/JwtFilter.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/jwt/JwtFilter.java
@@ -5,11 +5,14 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
