9b4729364a9b01ab2bee4cbdccae2ce795ceaf54
이창민 <cmlee0913@naver.com>
2023-01-27 05:12:51 +0900

    fix: try case

--- backend/openvidu-react/src/App.js
+++ backend/openvidu-react/src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 import './App.css';
 import UserVideoComponent from './UserVideoComponent';
 
-const APPLICATION_SERVER_URL = "http://i8d205.p.ssafy.io:10080/video/";
+const APPLICATION_SERVER_URL = "http://i8d205.p.ssafy.io/api/video/";
 
 
 class App extends Component {
