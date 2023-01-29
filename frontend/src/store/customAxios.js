85c5f5040eef98d2bc2742509fd799f197aa2833
Sea-cotton <flianous2@gmail.com>
2023-01-29 21:31:51 +0900

    feat:customAxios baseURL 수정

--- frontend/src/store/customAxios.js
+++ frontend/src/store/customAxios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const customAxios = axios.create({
   baseURL: "http://localhost:8080/",
+  // baseURL: "http://i8d205.p.ssafy.io:80/",
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
   },
