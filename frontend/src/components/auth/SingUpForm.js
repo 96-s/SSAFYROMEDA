a4d9c47c99231197ff868b88801bfae85ebd1781
이창민 <cmlee0913@naver.com>
2023-01-30 13:51:15 +0900

    rename: edit button.js

--- frontend/src/components/auth/SingUpForm.js
+++ frontend/src/components/auth/SingUpForm.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { authActions } from "../../store/AuthSlice";
 import { useNavigate } from "react-router-dom";
-import MyButton from "components/common/Button.js";
+import MyButton from "components/common/Button";
 
 const SignUpForm = () => {
   const [error, setError] = useState(null);
