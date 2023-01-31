386924d3bf2eca575aeece9da753cc680e5d9f8b
dpdms2148 <dpdms2148@naver.com>
2023-02-01 08:39:49 +0900

    feat : web mvc configuration 파일 작성

---
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.ssafy.sfrmd.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:3000/**").allowedHeaders("*")
+            .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
+                HttpMethod.PATCH.name())
+            .maxAge(1800);
+    }
+}
