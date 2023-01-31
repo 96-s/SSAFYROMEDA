535e4818a5aaadcf154048dc98871bbe7e6067e9
haesom <fliamento@naver.com>
2023-01-31 09:18:47 +0900

    bug:import 구문 추가

--- frontend/src/components/auth/SingUpForm.js
+++ frontend/src/components/auth/SingUpForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { authActions } from "../../store/AuthSlice";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import MyButton from "components/common/Button";
 import { parseJwt } from "components/utils/ParseJwt";
 
