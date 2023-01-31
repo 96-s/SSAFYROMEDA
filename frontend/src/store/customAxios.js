0109ce5dd6c57cae55d1d169cc3b92bdf92bbc32
haesom <fliamento@naver.com>
2023-01-31 17:49:53 +0900

    feat:닉네임 수정 및 로그인기능 구현

--- frontend/src/store/customAxios.js
+++ frontend/src/store/customAxios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const customAxios = axios.create({
-  baseURL: "http://192.168.100.139/",
-  // baseURL: "http://i8d205.p.ssafy.io:80/api/v1",
+  baseURL: "http://192.168.100.139:8080/",
+  // baseURL: "https://i8d205.p.ssafy.io/api/",
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
   },
