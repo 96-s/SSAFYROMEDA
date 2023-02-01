ab62b7c3cce49b32278e2839f1d62a221faee3d8
dpdms2148 <dpdms2148@naver.com>
2023-02-01 17:57:50 +0900

    feat : 배포서버 요청 허용

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/config/WebMvcConfig.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/config/WebMvcConfig.java
@@ -10,6 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:3000/**").allowedHeaders("*")
+            .allowedOrigins("https://i8d205.p.ssafy.io", "https://i8d205.p.ssafy.io/**").allowedHeaders("*")
             .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
                 HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
                 HttpMethod.PATCH.name())
