d80140eb57a1001e0f38845a201f7ae27c7245a2
이창민 <cmlee0913@naver.com>
2023-02-01 16:50:30 +0900

    fix: 오타수정

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/api/service/VideoService.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/api/service/VideoService.java
@@ -1,51 +1,51 @@
-package com.ssafy.sfrmd.api.service;
-
-import io.openvidu.java.client.Connection;
-import io.openvidu.java.client.ConnectionProperties;
-import io.openvidu.java.client.ConnectionType;
-import io.openvidu.java.client.OpenVidu;
-import io.openvidu.java.client.OpenViduHttpException;
-import io.openvidu.java.client.OpenViduJavaClientException;
-import io.openvidu.java.client.OpenViduRole;
-import io.openvidu.java.client.Session;
-import io.openvidu.java.client.SessionProperties;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@RequiredArgsConstructor
-@Getter
-@Service
-public class VideoService {
-    private final OpenVidu openVidu;
-
-    public Session getVideoSession() {
-        SessionProperties properties = new SessionProperties.Builder().build();
-
-        try {
-            return openVidu.createSession(properties);
-        } catch (OpenViduJavaClientException ex) {
-            throw new RuntimeException(ex);
-        } catch (OpenViduHttpException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    public Connection getVideoConnection() {
-        ConnectionProperties connectionProperties = new ConnectionProperties.Builder()
-            .type(ConnectionType.WEBRTC).role(OpenViduRole.PUBLISHER)
-            .data("user_data").build();
-
-        try {
-            return getVideoSession().createConnection(connectionProperties);
-        } catch(OpenViduJavaClientException e) {
-            throw new RuntimeException(e);
-        } catch(OpenViduHttpException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public String getVideoToken() {
-        return getVideoConnection().getToken();
-    }
-}
+//package com.ssafy.sfrmd.api.service;
+//
+//import io.openvidu.java.client.Connection;
+//import io.openvidu.java.client.ConnectionProperties;
+//import io.openvidu.java.client.ConnectionType;
+//import io.openvidu.java.client.OpenVidu;
+//import io.openvidu.java.client.OpenViduHttpException;
+//import io.openvidu.java.client.OpenViduJavaClientException;
+//import io.openvidu.java.client.OpenViduRole;
+//import io.openvidu.java.client.Session;
+//import io.openvidu.java.client.SessionProperties;
+//import lombok.Getter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@RequiredArgsConstructor
+//@Getter
+//@Service
+//public class VideoService {
+//    private final OpenVidu openVidu;
+//
+//    public Session getVideoSession() {
+//        SessionProperties properties = new SessionProperties.Builder().build();
+//
+//        try {
+//            return openVidu.createSession(properties);
+//        } catch (OpenViduJavaClientException ex) {
+//            throw new RuntimeException(ex);
+//        } catch (OpenViduHttpException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//    }
+//
+//    public Connection getVideoConnection() {
+//        ConnectionProperties connectionProperties = new ConnectionProperties.Builder()
+//            .type(ConnectionType.WEBRTC).role(OpenViduRole.PUBLISHER)
+//            .data("user_data").build();
+//
+//        try {
+//            return getVideoSession().createConnection(connectionProperties);
+//        } catch(OpenViduJavaClientException e) {
+//            throw new RuntimeException(e);
+//        } catch(OpenViduHttpException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public String getVideoToken() {
+//        return getVideoConnection().getToken();
+//    }
+//}
