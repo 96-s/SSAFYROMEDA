fab45a19fcad0ca9514fdca710543195a924d37e
haesom <fliamento@naver.com>
2023-02-02 09:54:45 +0900

    bug: token 저장 로직 추가

--- frontend/src/components/auth/OauthRedirect.js
+++ frontend/src/components/auth/OauthRedirect.js
@@ -15,9 +15,9 @@ const OauthRedirect = () => {
   //   const email = useSelector((state) => state.auth.register.userEmail);
 
   useEffect(() => {
-    // 받아온 이메일을 redux에 저장
+    // 받아온 이메일과 토큰을 redux에 저장
     if (userEmail) {
-      dispatch(authActions.addUserEmail(userEmail));
+      dispatch(authActions.addUserEmail({ token, userEmail }));
       console.log("이메밀 저장 성공");
       navigate("/signup");
     }
