944943e43f1077122e46405ba6a4da9e4277ea0c
dpdms2148 <dpdms2148@naver.com>
2023-02-02 15:24:34 +0900

    feat : history entitiy 빌더 및 메서드 추가

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/api/domain/history/History.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/api/domain/history/History.java
@@ -7,23 +7,45 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name="history")
+@Table(name = "history")
 public class History {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_no")
+    @Column(name = "user_no")
     private Long userNo;
 
-    @Column(name="history_play_count")
+    @Column(name = "history_play_count")
     private int historyPlayCount;
-    @Column(name="history_win_count")
+    @Column(name = "history_win_count")
     private int historyWinCount;
-    @Column(name="history_lose_count")
+    @Column(name = "history_lose_count")
     private int historyLoseCount;
+
+    @Builder
+    public History(Long userNo, int historyPlayCount, int historyWinCount, int historyLoseCount) {
+        this.userNo = userNo;
+        this.historyPlayCount = historyPlayCount;
+        this.historyWinCount = historyWinCount;
+        this.historyLoseCount = historyLoseCount;
+    }
+
+    public void updateHistoryPlayCount() {
+        this.historyPlayCount += 1;
+    }
+
+    public void updateHistoryWinCount() {
+        this.historyWinCount += 1;
+    }
+
+    public void updateHistoryLoseCount() {
+        this.historyLoseCount += 1;
+    }
 }
