3ced92b60582fb6c58bc40f756331544791a81d8
이창민 <cmlee0913@naver.com>
2023-02-03 14:07:32 +0900

    feat : 오픈비두 로직 생성

--- frontend/src/components/OpenviduTest/Openvidu.js
+++ frontend/src/components/OpenviduTest/Openvidu.js
@@ -29,7 +29,8 @@ class Openvidu extends Component {
       isChat: false,
     };
 
-    this.joinSession = this.joinSession.bind(this);
+    this.initRoom = this.initRoom.bind(this);
+    // this.joinSession = this.joinSession.bind(this);
     this.leaveSession = this.leaveSession.bind(this);
     // this.switchCamera = this.switchCamera.bind(this);
     // this.handleChangeSessionId = this.handleChangeSessionId.bind(this);
@@ -110,7 +111,7 @@ class Openvidu extends Component {
     }
   }
 
-  joinSession() {
+  initRoom() {
     // --- 1) Get an OpenVidu object ---
 
     this.OV = new OpenVidu();
@@ -161,7 +162,102 @@ class Openvidu extends Component {
         // --- 4) Connect to the session with a valid user token ---
 
         // Get a token from the OpenVidu deployment
-        this.getToken().then((token) => {
+        this.host().then((token) => {
+          // First param is the token got from the OpenVidu deployment. Second param can be retrieved by every user on event
+          // 'streamCreated' (property Stream.connection.data), and will be appended to DOM as the user's nickname
+          mySession
+            .connect(token, { clientData: this.state.myUserName })
+            .then(async () => {
+              // --- 5) Get your own camera stream ---
+
+              // Init a publisher passing undefined as targetElement (we don't want OpenVidu to insert a video
+              // element: we will manage it on our own) and with the desired properties
+              let publisher = await this.OV.initPublisherAsync(undefined, {
+                audioSource: undefined, // The source of audio. If undefined default microphone
+                videoSource: undefined, // The source of video. If undefined default webcam
+                publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+                publishVideo: true, // Whether you want to start publishing with your video enabled or not
+                resolution: "640x480", // The resolution of your video
+                frameRate: 30, // The frame rate of your video
+                insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
+                mirror: false, // Whether to mirror your local video or not
+              });
+
+              // --- 6) Publish your stream ---
+
+              mySession.publish(publisher);
+
+              // Obtain the current video device in use
+              var devices = await this.OV.getDevices();
+              var videoDevices = devices.filter(
+                (device) => device.kind === "videoinput"
+              );
+              var currentVideoDeviceId = publisher.stream
+                .getMediaStream()
+                .getVideoTracks()[0]
+                .getSettings().deviceId;
+              var currentVideoDevice = videoDevices.find(
+                (device) => device.deviceId === currentVideoDeviceId
+              );
+
+              // Set the main video in the page to display our webcam and store our Publisher
+              this.setState({
+                currentVideoDevice: currentVideoDevice,
+                mainStreamManager: publisher,
+                publisher: publisher,
+              });
+            })
+            .catch((error) => {
+              console.log(
+                "There was an error connecting to the session:",
+                error.code,
+                error.message
+              );
+            });
+        });
+      }
+    );
+  }
+
+  joinRoom(code) {
+    this.setState(
+      {
+        session: this.OV.initSession(),
+      },
+      () => {
+        let mySession = this.state.session;
+
+        // --- 3) Specify the actions when events take place in the session ---
+
+        // Session 객체가 각각 새로운 stream에 대해 구독 후, subscribers 상태값 업뎃
+        mySession.on("streamCreated", (event) => {
+          // OpenVidu -> Session -> UserVideoComponent를 사용하기 때문에 2번째 인자로 HTML
+          // 요소 삽입X
+          let subscriber = mySession.subscribe(event.stream, undefined);
+          var subscribers = this.state.subscribers;
+          subscribers.push(subscriber);
+
+          // Update the state with the new subscribers
+          this.setState({
+            subscribers: subscribers,
+          });
+        });
+
+        // 사용자가 화상회의를 떠나면 Session 객체에서 소멸된 stream을 받아와 subscribers 상태값 업뎃
+        mySession.on("streamDestroyed", (event) => {
+          // Remove the stream from 'subscribers' array
+          this.deleteSubscriber(event.stream.streamManager);
+        });
+
+        // On every asynchronous exception...
+        mySession.on("exception", (exception) => {
+          console.warn(exception);
+        });
+
+        // --- 4) Connect to the session with a valid user token ---
+
+        // Get a token from the OpenVidu deployment
+        this.guest(this.state.mySessionId).then((token) => {
           // First param is the token got from the OpenVidu deployment. Second param can be retrieved by every user on event
           // 'streamCreated' (property Stream.connection.data), and will be appended to DOM as the user's nickname
           mySession
@@ -287,20 +383,19 @@ class Openvidu extends Component {
           <div id="join">
             <div id="join-dialog" className="jumbotron vertical-center">
               <h1> Join a video session </h1>
-              <form className="form-group" onSubmit={this.joinSession}>
-                <p>
-                  <label>Participant: </label>
+              <form className="form-group" onSubmit={this.initRoom}>
+                <p className="text-center">
                   <input
-                    className="form-control"
-                    type="text"
-                    id="userName"
-                    value={myUserName}
-                    onChange={this.handleChangeUserName}
-                    required
+                    className="btn btn-lg btn-success"
+                    name="commit"
+                    type="submit"
+                    value="INIT"
                   />
                 </p>
+              </form>
+              <form className="form-group" onSubmit={this.joinRoom}>
                 <p>
-                  <label> Session: </label>
+                  <label> Code: </label>
                   <input
                     className="form-control"
                     type="text"
@@ -321,6 +416,7 @@ class Openvidu extends Component {
               </form>
             </div>
           </div>
+          
         ) : null}
 
         {this.state.session !== undefined ? (
@@ -393,15 +489,20 @@ class Openvidu extends Component {
    * Visit https://docs.openvidu.io/en/stable/application-server to learn
    * more about the integration of OpenVidu in your application server.
    */
-  async getToken() {
-    const sessionId = await this.createSession(this.state.mySessionId);
+  async host() {
+    const sessionId = await this.createSession();
+    this.state.mySessionId = sessionId;
     return await this.createToken(sessionId);
   }
 
-  async createSession(sessionId) {
+  async guest(sessionId) {
+    return await this.createToken(sessionId);
+  }
+
+  async createSession() {
     const response = await axios.post(
       APPLICATION_SERVER_URL,
-      { customSessionId: sessionId },
+      {},
       {
         headers: { "Content-Type": "application/json" },
       }
@@ -413,8 +514,7 @@ class Openvidu extends Component {
     const response = await axios.post(
       APPLICATION_SERVER_URL + "connect/" + sessionId,
       JSON.stringify({
-        userNo: 111,
-        userNickname: this.state.myUserName
+        sessionId: this.sessionId
       }),
       {
         headers: { "Content-Type": "application/json" },
