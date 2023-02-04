0f9aa7bc52d15fd1ec79a2fb63355149cc09acb5
jungeun park <pyooooong@gmail.com>
2023-02-04 12:53:38 +0900

    design: 설명 페이지 완성

--- frontend/src/pages/ExplanationPage.js
+++ frontend/src/pages/ExplanationPage.js
@@ -2,27 +2,34 @@ import styled from "styled-components";
 import TypingText from "components/utils/Typing";
 import MyButton from "components/common/Button";
 import { useNavigate } from "react-router";
+import Explanation_IMG from "resources/images/Explanation_IMG.png";
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  border: 1px solid black;
-  border-radius: 20px;
+//   border: 1px solid black;
+//   border-radius: 20px;
   margin: 19.5px;
-  width: 50%
+  width: 20vw;
+  position: relative;
+  top: 28%;
+  left: -0.5%;
 `;
 
 const Page = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+//   justify-content: center;
   align-items: center;
-  height: 100%;
-  width: 100%;
+  width: 100vw;
+  height: 100vh;
+  background: url(${Explanation_IMG}) no-repeat center;
+  background-size: 100% 100%;
 `;
 
 const ButtonDiv = styled.div`
-
+    position: absolute;
+    top: 50%;
 `;
 
 
@@ -37,11 +44,7 @@ const ExplanationPage = () => {
     return (
       <Page>
         <Container>
-            <TypingText text="싸피로메다 행성에 외계인들이 침공했다.
-            이들은 우주인들을 자신들과 같은 종족으로 만들어버리는 무시무시한 외계인들이다!
-            아직 외계인들이 침공하지 않은 평화로운 지구로 도망가야한다.
-            지구에 도착할 수 있는 우주선은 단 1대.
-            가장 먼저 지구에 도착해야한다." speed={60} fontSize="1.25rem" color="black" />
+            <TypingText text="[속보] 현재 미지의 초록색 외생명체 침입 중. 지금 당장 자매행성 지구로 탈출하길 바람." speed={60} fontSize="1.25rem" color="white" />
         </Container>
         <ButtonDiv>
             <MyButton
