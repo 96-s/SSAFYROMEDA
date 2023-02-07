603f63f40f71a88bdeefbebe1d574b7c91a22b3c
김성욱 <kimtjddnr2542naver.com>
2023-02-08 02:50:21 +0900

    refac : opnevidu initRoom() 함수형 컴포넌트로 리팩토링

--- frontend/src/pages/GamePage.js
+++ frontend/src/pages/GamePage.js
@@ -48,11 +48,15 @@ const GamePage = () => {
   const [isCamera, setIsCamera] = useState(true);
   const [isSpeaker, setIsSpeaker] = useState(true);
   const [myUserName, setMyUserName] = useState("");
+  const [currentVideoDevice, setCurrentVideoDevice]=useState(null);
 
   const getToken = async() => {
     const response = await axios.post(
       APPLICATION_SERVER_URL,
-      {},
+      {
+        userNo : 1,
+        userNickname : "abc",
+      },
       {
         withCredentials: true,
         headers: {
@@ -62,24 +66,30 @@ const GamePage = () => {
       }
     );
 
+    console.log(response);
     const mySessionId=response.data;
-      const res = await axios.post(
-        APPLICATION_SERVER_URL+mySessionId,
-        {},
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+
+    const res = await axios.post(
+      APPLICATION_SERVER_URL + mySessionId,
+      {},
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+      
     console.log("토큰 만듬");
+    console.log(res);
+    
     return res.data;    
   }
 
   useEffect(() => {
     const OV = new OpenVidu();
+
     OV.setAdvancedConfiguration({
       publisherSpeakingEventsOptions: {
         interval: 50,
@@ -87,37 +97,80 @@ const GamePage = () => {
       },
     });
 
-    setSessionId(OV.initSession());
+    // setSession(OV.initSession());
 
-    let mySession=session;
+    const mySession=OV.initSession();
     console.log("세션 생성 후");
+    console.log(mySession);
 
-    // mySession.on("streamCreated", (event) => {
-    //   // OpenVidu -> Session -> UserVideoComponent를 사용하기 때문에 2번째 인자로 HTML
-    //   // 요소 삽입X
-    //   let subscriber = mySession.subscribe(event.stream, undefined);
-    //   var subscribers = this.state.subscribers;
-    //   subscribers.push(subscriber);
+    mySession.on("streamCreated", (event) => {
+      // OpenVidu -> Session -> UserVideoComponent를 사용하기 때문에 2번째 인자로 HTML
+      // 요소 삽입X
+      let subscriber = mySession.subscribe(event.stream, undefined);
+      var subscribers = this.state.subscribers;
+      subscribers.push(subscriber);
 
-    //   // Update the state with the new subscribers
-    //   setSubscribers(subscribers);
-    // });
+      // Update the state with the new subscribers
+      setSubscribers(subscribers);
+    });
 
-    // // 사용자가 화상회의를 떠나면 Session 객체에서 소멸된 stream을 받아와 subscribers 상태값 업뎃
-    // mySession.on("streamDestroyed", (event) => {
-    //   // Remove the stream from 'subscribers' array
-    //   this.deleteSubscriber(event.stream.streamManager);
-    // });
+    // 사용자가 화상회의를 떠나면 Session 객체에서 소멸된 stream을 받아와 subscribers 상태값 업뎃
+    mySession.on("streamDestroyed", (event) => {
+      // Remove the stream from 'subscribers' array
+      this.deleteSubscriber(event.stream.streamManager);
+    });
 
-    // // On every asynchronous exception...
-    // mySession.on("exception", (exception) => {
-    //   console.warn(exception);
-    // });
+    // On every asynchronous exception...
+    mySession.on("exception", (exception) => {
+      console.warn(exception);
+    });
 
-    const response = getToken();
-    const token=response.data;
-    console.log(token);
-    // mySession.connect(token, {clientData : my})
+    getToken().then((token) => {
+      mySession
+        .connect(token, { clientData :  "user1"})
+        .then(async () => {
+          // Init a publisher passing undefined as targetElement (we don't want OpenVidu to insert a video
+          // element: we will manage it on our own) and with the desired properties
+          let publisher = await this.OV.initPublisherAsync(undefined, {
+            audioSource: undefined, // The source of audio. If undefined default microphone
+            videoSource: undefined, // The source of video. If undefined default webcam
+            publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+            publishVideo: true, // Whether you want to start publishing with your video enabled or not
+            resolution: "251.2x188.4", // 해상도
+            frameRate: 30, // The frame rate of your video
+            insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
+            mirror: true, // 거울모드
+          });
+
+          mySession.publish(publisher);
+          console.log("퍼블리시 후");
+          console.log(this.state);
+
+          // Obtain the current video device in use
+          var devices = await this.OV.getDevices();
+          var videoDevices = devices.filter(
+            (device) => device.kind === "videoinput"
+          );
+          var currentVideoDeviceId = publisher.stream
+            .getMediaStream()
+            .getVideoTracks()[0]
+            .getSettings().deviceId;
+          var currentVideoDevice = videoDevices.find(
+            (device) => device.deviceId === currentVideoDeviceId
+          );
+
+          setCurrentVideoDevice(currentVideoDevice);
+          setMainStreamManager(publisher);
+          setPublisher(publisher);
+        })
+        .catch((error) => {
+          console.log(
+            "There was an error connecting to the session:",
+            error.code,
+            error.message
+          );
+        });
+    });
 
   }, []);
 
