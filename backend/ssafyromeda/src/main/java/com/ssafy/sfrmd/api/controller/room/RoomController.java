cff01304f780023167ebfb7b06536ea5ddd0c6e4
dpdms2148 <dpdms2148@naver.com>
2023-02-07 10:43:50 +0900

    feat : room connect 연결 로직 수정

--- backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/api/controller/room/RoomController.java
+++ backend/ssafyromeda/src/main/java/com/ssafy/sfrmd/api/controller/room/RoomController.java
@@ -62,18 +62,15 @@ public class RoomController {
     }
 
 
-    @PostMapping("/connect/{sessionId}")
-    public ResponseEntity<?> createConnection(@PathVariable("sessionId") String sessionId)
-        // @RequestBody(required = false) RoomConnectRequest roomConnectRequest)
+    @PostMapping("/connect")
+    public ResponseEntity<?> createConnection(@RequestBody RoomConnectRequest roomConnectRequest)
         throws OpenViduJavaClientException, OpenViduHttpException {
 
-        // roomService.createConnection(roomConnectRequest, sessionId);
-
         Map<String, Object> params = new HashMap<>();
-        //params.put("userNo", roomConnectRequest.getUserNo());
-        //params.put("userNickname", roomConnectRequest.getUserNickname());
+        params.put("userNo", roomConnectRequest.getUserNo());
+        params.put("userNickname", roomConnectRequest.getUserNickname());
 
-        Session session = openvidu.getActiveSession(sessionId);
+        Session session = openvidu.getActiveSession(roomConnectRequest.getRoomCode());//sessionId
         if (session == null) {
             return new ResponseEntity<>(HttpStatus.valueOf(404));
         }
@@ -83,7 +80,6 @@ public class RoomController {
 
         return new ResponseEntity<>(connection.getToken(), HttpStatus.valueOf(200));
     }
-
 
 //    @PostMapping
 //    public ResponseEntity<? extends Object> createRoom(){
