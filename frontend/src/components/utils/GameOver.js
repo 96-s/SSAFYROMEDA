66d3ca3be60b41b6839da49a56ae94a5d0b9ac3f
jungeun park <pyooooong@gmail.com>
2023-02-14 23:32:54 +0900

    feat: 게임결과 페이지 애니메이션 구현

--- frontend/src/components/utils/GameOver.js
+++ frontend/src/components/utils/GameOver.js
@@ -1,17 +1,28 @@
 import defeatImg from "resources/images/defeat.png";
 import victoryImg from "resources/images/victory2.png";
 
+import styled from "styled-components";
+
+const GameOverContainer = styled.div`
+    animation: bounce 0.3s ease infinite alternate;
+    @keyframes bounce {
+        100% {
+          top: -20px;
+        }
+
+`;
+
 const GameOver = ({
     myTeam,
     winner,
     loser
 }) => {
     return (
-        <div>
+        <GameOverContainer>
             { myTeam === winner ?
                 <img src={victoryImg}></img>
             : <img src={defeatImg}></img> }
-        </div>
+        </GameOverContainer>
     );
 };
 
