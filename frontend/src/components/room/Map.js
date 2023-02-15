5e68de3522f23020475f83a4dd579c5d5e931edd
강모현 <rkdahgus12@naver.com>
2023-02-15 13:26:03 +0900

    feat; css 수정

--- frontend/src/components/room/Map.js
+++ frontend/src/components/room/Map.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Timer from "components/common/Timer";
+// import Timer from "components/common/Timer";
 // import DiceRoller from 'components/utils/DiceRoller';
 import DiceModal from "./DiceModal";
 import ChanceModal from "./ChanceModal";
-import Quiz from "./quiz";
+// import Quiz from "./quiz";
 
 // import Dice1 from "resources/images/Map/dice1.png";
 // import Dice2 from "resources/images/Map/dice2.png";
@@ -13,15 +13,6 @@ import Quiz from "./quiz";
 import MapIMG from "resources/images/Map/MapIMG.gif";
 import Marker1IMG from "resources/images/Map/marker1.png";
 import Marker2IMG from "resources/images/Map/marker2.png";
-import { scryRenderedComponentsWithType } from "react-dom/test-utils";
-// import MapFrame from "resources/images/mapframe.png";
-
-// const FrontimageDiv = styled.div`
-//   background: url(${MapFrame}) ;
-//   position: absolute;
-//   width: 600px;
-//   height: 500px;
-// `;
 
 const Page = styled.div`
   display: flex;
@@ -33,7 +24,7 @@ const Board = styled.div`
   position: relative;
   justify-content: center;
   margin: auto;
-  border: 1px solid black;
+  border: 1px solid white;
   aspect-ratio: 1 / 1;
   height: 85vh;
   background: url(${MapIMG}) no-repeat;
@@ -1326,6 +1317,7 @@ const Map = ({
   setTurnNum,
   myTurnNum,
   setIsGameOver,
+  myGameNo
 }) => {
   const [diceValue, setDiceValue] = useState(null);
   const [showDiceToggle, setShowDiceToggle] = useState(false);
@@ -1363,14 +1355,19 @@ const Map = ({
 
   // 주사위 굴릴 때마다 위치 이동
   useEffect(() => {
-    if (diceValue !== null) {
-      console.log("주사위 값은 " + diceValue);
-      // 주사위 1 나왔을 때
-      if (isRoll === false && diceValue === 1) {
+    console.log("주사위 값은 " + diceValue);
+    const arr = [0, 1, 2]
+    // 주사위 1 나왔을 때
+    if (isRoll === false && diceValue === 1) {
+      if (arr.includes(myTurnNum)) {
         setT1Pos(t1Pos + diceValue);
+      } else {
+        setT2Pos(t2Pos + diceValue)
       }
+    }
       // 주사위 2 이상
-      if (isRoll === false && (diceValue === 2 || diceValue === 3)) {
+    if (isRoll === false && (diceValue === 2 || diceValue === 3)) {
+      if (arr.includes(myTurnNum)) {
         console.log("왜 안뜨노...");
         var i = 0;
         while (i < diceValue) {
@@ -1380,12 +1377,30 @@ const Map = ({
             setT1Pos((t1Pos) => t1Pos + 1);
           }, 2000 * i);
         }
+      } else {
+        console.log("왜 안뜨노...");
+        var k = 0;
+        while (k < diceValue) {
+          k++;
+          console.log("왜 안됨");
+          setTimeout(() => {
+            setT2Pos((t2Pos) => t2Pos + 1);
+          }, 2000 * k);
+        }
       }
-      setDiceValue(null);
-      console.log("팀1자리" + t1Pos);
-      sendPos();
     }
-  }, [diceValue]);
+      setDiceValue(null);
+      console.log("1팀자리" + t1Pos);
+      console.log("2팀자리" + t2Pos);
+      if (arr.includes(turnNum)) {
+        setTurnNum((turnNum + 3) % 6);
+      } else if (turnNum === 5) {
+        setTurnNum(0);
+      } else {
+        setTurnNum((turnNum - 2) % 6);
+      }
+      sendPos();
+    }, [diceValue]);
 
   const closeDice = useEffect(() => {
     // console.log(diceValue);
@@ -1398,6 +1413,8 @@ const Map = ({
     }
   }, [diceValue]);
   // console.log(diceValue);
+
+  console.log("지금 순서는 누구?" + turnNum);
 
   return (
     <Page>
