0fd09f9d8791465d476f13c12e823afdb0324eb4
김성욱 <kimtjddnr2542naver.com>
2023-02-16 15:41:38 +0900

    rename : 필요없는 파일 삭제 및 수정

--- frontend/src/pages/GamePage.js
+++ frontend/src/pages/GamePage.js
@@ -1,7 +1,7 @@
 import GameManager from "components/Game/GameManager";
 import { useLocation } from "react-router-dom";
 
-const UiTest = () => {
+const GamePage = () => {
   const location = useLocation();
   // const sessionNickname = location.state.sessionNickname;
   // const sessionRoomId = location.state.sessionRoomId;
@@ -20,4 +20,4 @@ const UiTest = () => {
   );
 };
 
-export default UiTest;
+export default GamePage;
